9a13ddff31adc5887c6f6dbd565d051ee2c3d8c6
Woleen <marcin.dadon@gmail.com>
2024-03-23 21:51:32 +0100

    Added charts for test result

---
+++ Frontend/wellbeing-of-mind/src/user_specific/TestResultChart.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+
+const TestResultChart = ({ data }) => {
+  const chartData = {
+    labels: data.map(([choice]) => choice),
+    datasets: [{
+      label: 'Choice Counts',
+      data: data.map(([, count]) => count),
+      backgroundColor: [
+        'rgba(255, 0, 0, 0.8)',
+        'rgba(255, 255, 0, 0.8)',
+        'rgba(0, 128, 0, 0.8)',
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  return (
+    <div style={{ height: '40vh', margin: 'auto' }}>
+      <Pie
+        data={chartData}
+        options={{
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              position: 'bottom',
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default TestResultChart;
